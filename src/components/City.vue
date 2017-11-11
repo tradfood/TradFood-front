6140b1c81e763812ffebd1b0a4d8b74322c14693
Bastien <bastienrbt@protonmail.com>
2017-11-11 16:27:57 +0100

    Filters work - Result displayed in JS console

--- src/components/City.vue
+++ src/components/City.vue
@@ -3,13 +3,34 @@
     <h1>{{ name }}</h1>
     <p>{{ city.description }}</p>
     <router-link to="/">{{ $t('buttons.home') }}</router-link>
+    <br><br>
+
+    <label for="mealType">Type :</label>
+    <select v-model="filtersData.mealType" id="mealType" @change="sort()">
+      <option v-for="option in filtersOptions.mealType" v-bind:value="option.value">
+        {{ option.text }}
+      </option>
+    </select>
+    <br>
+    <label for="difficulty">Difficulté :</label>
+    <select v-model="filtersData.difficulty" id="difficulty" @change="sort()">
+      <option v-for="option in filtersOptions.difficulty" v-bind:value="option.value">
+        {{ option.text }}
+      </option>
+    </select>
+
+    <br><br>
+
     <ul v-for="recipe in city.recipes">
       <li>
         <router-link :to="{name: 'recipe', params: {city: city.url, recipe: recipe.url} }">
-          {{ recipe.name }}
+          {{ recipe.name }} |
+          {{ recipe.difficulty }} |
+          {{ recipe.meal_type }}
         </router-link>
       </li>
     </ul>
+    <br><br>
   </div>
 </template>
 
@@ -18,7 +39,25 @@ export default {
   filters: require('../mixins/Filters'),
   data: function () {
     return {
-      city: []
+      city: [],
+      filtersData: {
+        mealType: 'all',
+        difficulty: 'all'
+      },
+      filtersOptions: {
+        mealType: [
+          { text: 'Tout', value: 'all' },
+          { text: 'Entrée', value: 'starter' },
+          { text: 'Plat', value: 'main course' },
+          { text: 'Dessert', value: 'dessert' }
+        ],
+        difficulty: [
+          { text: 'Tout', value: 'all' },
+          { text: 'Facile', value: '1' },
+          { text: 'Moyen', value: '2' },
+          { text: 'Difficile', value: '3' }
+        ]
+      }
     }
   },
   mounted: function () {
@@ -29,10 +68,24 @@ export default {
       this.$router.push('/')
     })
   },
-  computed: {
-    name () {
-      const name = this.$route.params.city
-      return name.charAt(0).toUpperCase() + name.slice(1)
+  methods: {
+    sort: function () {
+      var filteredRecipes = []
+      var selectedDifficulty = this.filtersData.difficulty
+      var selectedMealType = this.filtersData.mealType
+
+      for (let i = 0; i < this.city.recipes.length; i++) {
+        if (
+          (this.city.recipes[i].difficulty === parseInt(selectedDifficulty) ||
+          selectedDifficulty === 'all') &&
+          (this.city.recipes[i].meal_type === selectedMealType ||
+          selectedMealType === 'all')
+        ) {
+          filteredRecipes.push(this.city.recipes[i])
+        }
+      }
+
+      console.log(filteredRecipes)
     }
   }
 }
