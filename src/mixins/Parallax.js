280903d310d8fb83429aba9455c969126df1b625
Bastien <bastienrbt@protonmail.com>
2017-11-23 15:24:27 +0100

    Add parallax mixin

---
+++ src/mixins/Parallax.js
@@ -0,0 +1,28 @@
+class Parallax {
+
+  init () {
+    this.els = []
+    this.els = document.querySelectorAll('[data-parallax]')
+    this.scrollHandler()
+    document.addEventListener('scroll', evt => this.scrollHandler(evt))
+    window.addEventListener('resize', evt => this.scrollHandler(evt))
+  }
+
+  scrollHandler () {
+    for (var i = 0; i < this.els.length; i++) {
+      var el = this.els[i]
+      var factor = parseInt(el.getAttribute('data-parallax')) || 1
+
+      var elRect = el.getBoundingClientRect()
+
+      var percent = Math.round((elRect.top + el.offsetHeight) / (window.innerHeight + el.offsetHeight) * 100)
+      percent = Math.max(0, Math.min(100, percent))
+
+      var translation = Math.round(factor * (percent - 50))
+
+      el.style.transform = 'translateY(' + translation + 'px)'
+    }
+  }
+}
+
+module.exports = new Parallax()
