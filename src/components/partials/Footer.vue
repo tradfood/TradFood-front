201a30abc2f9496f118a32be20c2d3b3776b02db
Bastien <bastienrobert@outlook.com>
2018-02-27 18:58:24 +0100

    Architecture for the footer

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,5 +1,26 @@
 <template>
-  <h4>Footer</h4>
+  <footer>
+    <div>
+      <h2>Abonnez-vous !</h2>
+      <form class="" action="index.html" method="post">
+        <input type="text" name="" value="" placeholder="MAILCHIMP MAIL">
+      </form>
+    </div>
+    <div>
+      <h2>Plus</h2>
+      <a href="#">Notre équipe</a>
+      <a href="#">Qui sommes-nous ?</a>
+    </div>
+    <div>
+      <h2>Contactez-nous</h2>
+      <a href="mailto:contact@tradfood.fr">contact@tradfood.fr</a>
+      <div class="social">
+        <div class="twitter"></div>
+        <div class="instagram"></div>
+        <div class="facebook"></div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
