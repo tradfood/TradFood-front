168014d6b96fba8d177d93eed6b3832d2921664f
Bastien <bastienrobert@outlook.com>
2018-02-28 01:30:18 +0100

    Add Mailchimp to the footer

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -2,9 +2,22 @@
   <footer>
     <div>
       <h2>Abonnez-vous !</h2>
-      <form class="" action="index.html" method="post">
-        <input type="text" name="" value="" placeholder="MAILCHIMP MAIL">
-      </form>
+      <div id="mc_embed_signup">
+        <form action="https://tradfood.us17.list-manage.com/subscribe/post?u=aeeb4785daf341646eb4140fa&amp;id=bf0f9b5385" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+            <div class="mc-field-group">
+            	<label for="mce-EMAIL">Email Address </label>
+            	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+            </div>
+          	<div id="mce-responses" class="clear">
+          		<div class="response" id="mce-error-response" style="display:none"></div>
+          		<div class="response" id="mce-success-response" style="display:none"></div>
+          	</div>
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_aeeb4785daf341646eb4140fa_bf0f9b5385" tabindex="-1" value=""></div>
+              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            </div>
+        </form>
+      </div>
     </div>
     <div>
       <h2>Plus</h2>
