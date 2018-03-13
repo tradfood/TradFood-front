94189d02b89e8a26b9d0b0e9b6cff289dabd7d11
Bastien <bastienrobert@outlook.com>
2018-03-13 23:16:21 +0100

    Implement some schema.org data for recipes

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -1,21 +1,34 @@
 <template>
   <div>
     <div class="container">
-      <div id="recipe">
+      <div id="recipe" itemscope itemtype="http://schema.org/Recipe">
+        <link itemprop="url" :href="'https://tradfood.fr' + this.$route.fullPath" />
+        <meta itemprop="mainEntityOfPage" content="True" />
         <div class="recipe-image">
-          <img :src="$options.filters.formatUrl(recipe.image.url)" :alt="recipe.name">
+          <img :src="$options.filters.formatUrl(recipe.image.url)" :alt="recipe.name" itemprop="image">
         </div>
         <div class="recipe-details">
-          <div class="recipe-title">
+          <div class="recipe-title" itemprop="name">
             <h1>{{ recipe.name }}</h1>
+          </div>
+          <div class="recipe-description">
+            <p>{{ recipe.description }}</p>
           </div>
           <div class="recipe-content">
             <div class="recipe-details__informations">
               <h2>Informations</h2>
               <ul>
-                <li>Nombre de pièces: {{ recipe.servings }}</li>
-                <li>Temps de préparation: {{ recipe.preparation_time }}</li>
-                <li>Temps de cuisson: {{ recipe.cooking_time }}</li>
+                <li itemprop="recipeYield">Nombre de pièces: {{ recipe.servings }}</li>
+                <li>
+                  <time itemprop="prepTime" :datetime="'PT' + recipe.preparation_time + 'M'">
+                    Temps de préparation: {{ recipe.preparation_time }}
+                  </time>
+                </li>
+                <li>
+                  <time itemprop="cookTime" :datetime="'PT' + recipe.cooking_time + 'M'">
+                    Temps de cuisson: {{ recipe.cooking_time }}
+                  </time>
+                </li>
                 <li>Type de plat: {{ $t(`recipe.filters.mealType.${recipe.meal_type.replace(/\s+/g, '-')}`) }}</li>
                 <li>Difficulté: {{ $t(`recipe.filters.difficulty.${recipe.difficulty}`) }}</li>
                 <li>Saison: {{ $t(`recipe.filters.season.${recipe.season}`) }}</li>
@@ -23,11 +36,11 @@
             </div>
             <div class="recipe-details__ingredients">
               <h2>Ingredients</h2>
-              <div v-html="$options.filters.compiledMarkdown(recipe.ingredients)" class="content"></div>
+              <div v-html="$options.filters.compiledMarkdown(recipe.ingredients)" class="content" itemprop="ingredients"></div>
             </div>
             <div class="recipe-details__recipe">
               <h2>Recette</h2>
-              <div v-html="$options.filters.compiledMarkdown(recipe.recipe)" class="content"></div>
+              <div v-html="$options.filters.compiledMarkdown(recipe.recipe)" class="content" itemprop="recipeInstructions"></div>
             </div>
           </div>
         </div>
